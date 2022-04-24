55d32de112974a8a15cd657d134c15049e4a92bb
Darcy <github@darcythomas.com>
2022-04-24 14:14:45 +1200

    Update create.ps1

--- src/generateScripts/create.ps1
+++ src/generateScripts/create.ps1
@@ -1,3 +1,5 @@
+
+
 param (
 
     [Parameter(Mandatory)]
@@ -7,42 +9,84 @@ param (
         }
         return $true
     })]
-    [System.IO.FileInfo]$path
+    [System.IO.FileInfo]
+    $path
     )
 
+    
+$name = "Coffee"
+$author = "Darcy"
+
+function Setup-Template {
+   
+$templateJson = @"
+    {
+        "`$schema": "http://json.schemastore.org/template",
+        "author": "$author",
+        "classifications": [ "Common", "Code" ],
+        "identity": "ExampleTemplate.StringExtensions",
+        "name": "Example templates: string extensions",
+        "shortName": "$name",
+        "tags": {
+          "language": "C#",
+          "type": "item"
+        }
+    }
+"@
+    
+    $folder =  ".template.config";
+    mkdir $folder
+    $templateJson | Out-File "$path\$folder\template.json"
+       
+}
+
+
+
+function Create-RootApp {
+
+    $folder =  "app";
+    dotnet new razor -o "$path\$folder" --name $name
+
+}
+function Create-Tests {
+
+    $folder =  "tests";
+    dotnet new xunit -o "$path$folder\unit" --name $name
+    dotnet new xunit -o "$path$folder\intergration" --name $name
+    dotnet new console -o "$path$folder\performance" --name $name
+
+    Push-Location
+    Set-Location "$path$folder\performance"
+    
+    dotnet add package  BenchmarkDotNet 
+    Pop-Location
+
+}
+
+function Create-ServiceProject {
+
+    $folder =  "services";
+    dotnet new classlib -o "$path\$folder" --name "$name.Services"
+
+}
+
+
+    
 
 Push-Location 
 Set-Location $path
 
-write-host "Starting $path"
 
-dotnet new razor
-$templateJson = @"
-{
-    "`$schema": "http://json.schemastore.org/template",
-    "author": "Me",
-    "classifications": [ "Common", "Code" ],
-    "identity": "ExampleTemplate.StringExtensions",
-    "name": "Example templates: string extensions",
-    "shortName": "stringext",
-    "tags": {
-      "language": "C#",
-      "type": "item"
-    }
-}
-"@
+Setup-Template
+Create-RootApp
+Create-Tests
+Create-ServiceProject
 
 
-$templateFolder =  ".template.config";
-mkdir $templateFolder
-$templateJson | Out-File "$path\$templateFolder\template.json"
-
-
-
-mkdir 'services'
 mkdir 'dataAccess'
-mkdir 'tests'
+
 mkdir 'frontend'
 
 Pop-Location
-#dotnet --help
+
+
