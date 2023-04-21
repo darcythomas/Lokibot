8c99fc56b96adf87c723d72d1c99b0a42ee41347
Darcy <github@darcythomas.com>
2023-04-21 14:42:11 +1200

    Update generate.ps1

--- generate.ps1
+++ generate.ps1
@@ -1,5 +1,6 @@
-$Path = "$PSScriptRoot\src\template\"
+$name = "Coffee"
+$Path = "$PSScriptRoot\src\template\$name"
 
 
 & "$PSScriptRoot\src\generateScripts\clean.ps1" -path "$Path"
-& "$PSScriptRoot\src\generateScripts\create.ps1" -path "$Path"
+& "$PSScriptRoot\src\generateScripts\create.ps1" -path "$Path" -name $name
